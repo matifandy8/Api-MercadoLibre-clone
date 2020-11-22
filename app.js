e566ea027a86d9170f56d4e5c3ec19df305fbdbb
matifandy <matifandy@gmail.com>
2020-11-22 20:04:04 -0300

    json

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const http = require("http");
 const express = require("express");
-var cors = require("cors");
+const cors = require("cors");
+const importData = require("./products.json");
 const app = express();
 
 app.use(cors());
 
 app.set("json spaces", 2);
 
+app.get("/products", (req, res) => {
+  res.send(importData);
+});
+
 app.get("/", function (req, res) {
   // Siempre la primer variable es la petición, la segunda la respuesta
 
   res.sendFile(__dirname + "/index.html");
 });
-
-// routes
-app.use("/api/products", require("./routes/products"));
 
 const server = http.createServer(app);
 const port = process.env.PORT || 1984;
